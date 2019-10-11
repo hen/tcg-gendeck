8c2e15b93cac69cfcb46c448d795a9aac75c5153
hen <dev@flamefew.com>
2019-10-11 10:58:23 -0700

    Split the Wave 4 Weaponizers up

--- py/get-json.py
+++ py/get-json.py
@@ -1,17 +1,40 @@
 import json
 import os
+import re
+import copy
 
 from airtable import Airtable
 
 # Grab the cards, save them as JSON
-def airtable_to_json_file(id, api_key, table_name, file_name):
+def airtable_to_json_file(id, api_key, table_name, file_name, mutator = None):
     # Open a connection to Airtable
     at = Airtable(id, table_name, api_key=api_key)
     cards = at.get_all()
+    if(mutator):
+        cards = mutator(cards)
     data = { "records" : cards }
     json_file=open(file_name, 'w+')
     json_file.write(json.dumps(data, indent=2))
     json_file.close()
+
+# Fix the Wave 4 Weaponizers so they have two entires instead of one
+def bot_mutate(cards):
+    for card in cards:
+        # If Card # contains an &, then split it into two cards
+        if('&' in card['fields']['Card #']):
+            # Copy the card
+            copied_card=copy.deepcopy(card)
+            # Fix the copy's Card #
+            copied_card['fields']['Card #'] = re.sub(r"T..&", '', copied_card['fields']['Card #'])
+            # Fix the copy's images
+            copied_card['fields']['Image(s)'] = [ copied_card['fields']['Image(s)'][2], copied_card['fields']['Image(s)'][1] ]
+            # Add the copy
+            cards.append(copied_card)
+            # Fix the original's Card #
+            card['fields']['Card #'] = re.sub(r"&T..", '', card['fields']['Card #'])
+            # Fix the original's images
+            card['fields']['Image(s)'] = [ card['fields']['Image(s)'][2], card['fields']['Image(s)'][0] ]
+    return cards
 
 # Load the airtable key from disk
 key_file=open('airtable.key', 'r')
@@ -22,6 +45,6 @@ key_file.close()
 transformers_card_id='appVjmNG3AukyOaQn'
 
 # Grab cards
-airtable_to_json_file(transformers_card_id, key, 'Bot Cards', 'docs/json/bot-cards.json')
+airtable_to_json_file(transformers_card_id, key, 'Bot Cards', 'docs/json/bot-cards.json', bot_mutate)
 airtable_to_json_file(transformers_card_id, key, 'Battle Cards', 'docs/json/battle-cards.json')
 airtable_to_json_file(transformers_card_id, key, 'Combiner Forms', 'docs/json/combiner-forms.json')
